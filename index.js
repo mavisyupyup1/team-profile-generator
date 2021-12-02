afd4389550637c43e1ec88306d2ea5509c4dfe36
mavisyupyup1 <mavisyupyup1@github.com>
2021-12-01 22:58:41 -0500

    more files

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const inquirer = require ('inquirer');
 const Employee =require('./lib/Employee')
+const Manager = require('./lib/Manager')
 class Team {
     constructor(){
         this.currentEmployee;
         this.members =[];
     }
 }
-module.exports =Team;
+function appMenu(){
+    function initialManager(){
+        console.log("please built your team:")
+    }
+    function createTeam(){
+        inquirer.prompt
+
+        //call engineer func->ask for engineer->create engineer ->
+        //call intern func
+        //bye(call gen html func)
+
+    }
+    function createEngineer(){
+
+    }
+    function createIntern(){
+
+
+    }
+    //build team.html
+    function buildTeam(){
+
+    }
+}
+//start
+appMenu()
+console.log(__dirname)
+const OUTPUT_DIR = path.resolve(__dirname,"output")
+const outputPath = path.join(OUTPUT_DIR,"team.html")
+const render = require()
