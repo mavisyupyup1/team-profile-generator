e972cfd80276b58783d06f5d30661c7f46f8e045
Your Name <graceliu@gracelius-iMac.home>
2021-12-03 10:02:00 -0500

    pass employee test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,9 +1,24 @@
 const Employee =require('../lib/Employee')
 
-test ('create an employee object',()=>{
+test ('creates an employee object',()=>{
+    const employee = new Employee ('Roger',2,'roger@sad.com');
+    expect(employee.name).toBe('Roger');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+
+})
+
+test("gets employee's name",()=>{
     const employee = new Employee ('Roger');
     expect(employee.name).toBe('Roger');
-    // expect(employee.id).toBe(expect.any(Number));
-    // expect(employee.email).toBe(expect.any(String));
+})
 
+test("gets employee Id",()=>{
+    const employee =new Employee('Roger',2)
+    expect(employee.id).toEqual(2)
+})
+
+test("gets employee role",()=>{
+    const employee = new Employee('Roger');
+    expect(employee.getRole()).toBe('Employee')
 })
