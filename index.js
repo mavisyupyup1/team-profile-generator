8ff67fa7b42c68ae8976c59e06b1214ae53a970a
mavisyupyup1 <mavisyupyup1@github.com>
2021-12-04 10:30:12 -0500

    adding html templates

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const fs = require('fs')
 
 const OUTPUT_DIR = path.resolve(__dirname,"output")
 const outputPath = path.join(OUTPUT_DIR,"team.html")
-const generateTeam = require('./src/template')
+const render = require('./lib/render')
 
 const teamMembers=[];
 const idArray =[]
@@ -43,14 +43,13 @@ function appMenu(){
                 case 'Intern':
                 createIntern();
                 break;
-                //bye(call gen html func)
+                //bye(call gen html function)
                 case 'I am done adding team members':
-                generateTeam().then((templateToSave)=>{
-                    fs.writeFile('./dist/index.html',templateToSave)
-                });
-                break;
-            }
-    })
+                    generateTeam()
+            } 
+        })
+           
+   
     }
     function createEngineer(){
         inquirer.prompt(engineerQuestions)
@@ -72,6 +71,10 @@ function appMenu(){
         })
     }
   
+}
+
+//generateTeam function call the "render" function (required above) and pass in the teamMembers array containing all employee object. once it's rendered, the returned block of html will be saved in an teamMember.html file saved in the outputPath (specified above) location.
+function generateTeam(){fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
 }
 //start
 appMenu()
